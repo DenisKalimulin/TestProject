8c6c31d06695f358526d82d20557b767faa4b228
DenisKalimulin <159532816+DenisKalimulin@users.noreply.github.com>
2024-04-18 15:59:52 +0300

    3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
-public class HelloWOrld {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello Java!");
+        System.out.println("Hello WORLD!!!");
     }
 }
